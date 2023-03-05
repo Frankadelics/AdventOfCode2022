b543eeb788e5141f6b67ce35068eda0b7bc1bf0e
Francisco Arevalo <arevalofrancisco77@gmail.com>
2023-03-05 16:21:18 -0700

    Getting inputs and .cpp file ready.

---
+++ day2/day2.cpp
@@ -0,0 +1,28 @@
+/*
+    A - Rock
+    B - Paper
+    C - Scissors
+
+    X - Choose Rock
+    Y - Choose Paper
+    Z - Choose Scissors
+
+    Points Awarded for Shape
+    1  - Rock
+    2  - Paper
+    3  - Scissors 
+
+    Points Awarded for Outcome
+    0 - Lose
+    3 - Draw
+    6 - Win
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
