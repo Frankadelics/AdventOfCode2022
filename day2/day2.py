952f6f2715995a83d8cb801a3d118b8b6dd174d9
Francisco Arevalo <arevalofrancisco77@gmail.com>
2023-03-06 22:11:04 -0700

    Commit is to move work over to the rpi4

---
+++ day2/day2.py
@@ -0,0 +1,16 @@
+def tallyPointsPart1():
+    
+def tallyPointsPart2():
+
+def getList(file_path, part):
+    with open(file_path, 'r') as file:
+        if part == 1:
+            tallyPointsPart1()
+        else:
+            tallyPointsPart2() 
+
+def main():
+    print()
+
+if __name__ == "__main__":
+    main()
